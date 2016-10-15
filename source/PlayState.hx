aa9bf9fd42d96ee52ac1937e3d28d602fca5a755
Yusuf Fahry <moshefeit@users.noreply.github.com>
2016-10-15 17:58:22 +0700

    Update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -11,31 +11,25 @@ import flixel.util.FlxColor;
 class PlayState extends FlxState
 {
 	private var laler:Player;
-	private var laler1:Player;
-	private var laler2:Player;
+
 	
 	override public function create():Void
 	{
 		super.create();
 		
 		laler = new Player(FlxColor.GREEN, 90);
-		laler1 = new Player(FlxColor.YELLOW, 0);
-		laler2 = new Player(FlxColor.RED, 180);
-		
+
 		add(laler);
-		add(laler1);
-		add(laler2);
 	}
 
 	override public function update(elapsed:Float):Void
 	{
-		super.update(elapsed);
-		
 		if (FlxG.keys.justPressed.ENTER)
 		{
-			laler.allowed = 1;
-			laler1.allowed = 1;
-			laler2.allowed = 1;			
+			laler.allowed = 1;		
 		}
+		
+		super.update(elapsed);
+		laler.allowed = 0;
 	}
 }
